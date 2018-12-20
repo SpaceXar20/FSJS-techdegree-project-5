feaee61247f8cb65930b55dce92c73a5eaf2a837
Erk L <ericklo1094011@gmail.com>
2018-12-19 21:38:13 -0500

    Renamed a variable
    I renamed the variable in line 18 for better readability

--- js/app.js
+++ js/app.js
@@ -12,20 +12,13 @@ $.ajax({
     success: function(data) {
       console.log(data.results); //this should log the data for 12 employees inJSON format
 
-      /*create a search bar dynamically
-      use the following markup as a reference, append it to `search-container` div.
+      /*starting from line 25, I created a search bar dynamically, I
+      used the HTML markup as a reference, I append it to `search-container` div.*/
 
-      <form action="#" method="get">
-                            <input type="search" id="search-input" class="search-input" placeholder="Search...">
-                            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-                        </form>
-      
-      */
-
-      var $formAndMethod = $('<form action="#" method="get"> </form>')
-      $formAndMethod.append('<input type="search" id="search-input" class="search-input" placeholder="Search...">')
-      $formAndMethod.append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">')
-      $('.search-container').append($formAndMethod);
+      var $searchBox = $('<form action="#" method="get"> </form>')
+      $searchBox.append('<input type="search" id="search-input" class="search-input" placeholder="Search...">')
+      $searchBox.append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">')
+      $('.search-container').append($searchBox);
     }
     
   });
