f7f915d74d4487bdaa586066aa4e9bda04e9b3d9
Erk L <ericklo1094011@gmail.com>
2018-12-24 11:57:38 -0500

    Resolved issue with displaying random pics
    I was able to resolve the issue I had, I was getting the same pictures because I ha used the clone() on the cards,

--- js/app.js
+++ js/app.js
@@ -16,93 +16,39 @@
       /*Next I will create the gallery markup by using the commented html markup as reference and
        append them to the `gallery` div. */
 
-      //create variables for DIV Elements
-      var $galleryMarkUp = $('<div class="card"> </div>');
-      var $cardIMGContainer = $('<div class="card-img-container"> </div>');
-      var $cardInfoContainer = $('<div class="card-info-container"> </div>');
+/*This commented out code is what I originally had but instead of randomly showing 12 employees, the pictures showed 
+the same person, OI corrected this near the ajax request*/
 
-      //Append $cardIMGContainer and $cardInfoContainer inside $galleryMarkUp
-      $galleryMarkUp.append($cardIMGContainer, $cardInfoContainer);
+       //create variables for DIV Elements
+//       var $galleryMarkUp = $('<div class="card"> </div>');
+//       var $cardIMGContainer = $('<div class="card-img-container"> </div>');
+//       var $cardInfoContainer = $('<div class="card-info-container"> </div>');
 
-      //I append an img src inside $cardIMGContainer
-      $cardIMGContainer.prepend('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">');
+//       //Append $cardIMGContainer and $cardInfoContainer inside $galleryMarkUp
+//       $galleryMarkUp.append($cardIMGContainer, $cardInfoContainer);
+
+//       //I append an img src inside $cardIMGContainer
+//       $cardIMGContainer.prepend('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">');
       
-      //I also need to append an h3 and a couple of paragraphs inside $cardInfoContainer
-      $cardInfoContainer.prepend('<h3 id="name" class="card-name cap">first last</h3>',
-      '<p class="card-text">email</p>', '<p class="card-text cap">city, state</p>');
+//       //I also need to append an h3 and a couple of paragraphs inside $cardInfoContainer
+//       $cardInfoContainer.prepend('<h3 id="name" class="card-name cap">first last</h3>',
+//       '<p class="card-text">email</p>', '<p class="card-text cap">city, state</p>');
       
-    //Append $galleryMarkUp inside <div id="gallery" class="gallery">
-     $('#gallery').append($galleryMarkUp);
+//     //Append $galleryMarkUp inside <div id="gallery" class="gallery">
+//      $('#gallery').append($galleryMarkUp);
      
     
      
-/*I need to create 12 gallery cards in order to hold 12 employees 
-     I used a code snippet from https://stackoverflow.com/a/10426334/10043628 user Guffa
-     */
+// /*I need to create 12 gallery cards in order to hold 12 employees 
+//      I used a code snippet from https://stackoverflow.com/a/10426334/10043628 user Guffa
+//      */
      
-    for (let index = 0; index <11; index++) {
-       $galleryMarkUp.clone().insertAfter($galleryMarkUp)
+//     for (let index = 0; index <11; index++) {
+//        $galleryMarkUp.clone().insertAfter($galleryMarkUp)
         
-    }
+//     }
 //==========================================================================================================================================
 
-     /*Now I will dynamically create the modal box markup using the html below as guidance*/ 
-    
-     //Create variables for the div elements
-     var $modalBoxContainer = $('<div class="modal-container"> </div>');
-     var $modal = $('<div class="modal"> </div>');
-     var $modalInfoContainer = $('<div class="modal-info-container"> </div>');
-     var $modalBtnContainer = $('<div class="modal-btn-container"> </div>'); //modal buttons
-
-     //Append $modal, $modalInfoContainer and $modalBtnContainer inside $modalBoxContainer
-      $modalBoxContainer.append($modal, $modalInfoContainer,);
-      
-      //Append $modalBtnContainer, inside $modal
-      $modal.append('<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong> </button');
-
-
-      /*Inside $modal I will append a 
-      [img scr], 
-      [h3], 
-      [2 paragraphs], 
-      followed by an [hr], 
-      and 3 more paragraphs 
-      and lastly $modalBtnContainer */   
-
-      $modal.prepend('<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">', 
-      '<h3 id="name" class="modal-name cap">name</h3>', 
-      '<p class="modal-text">email</p>',
-       '<p class="modal-text cap">city</p>',
-      '<hr>', 
-      '<p class="modal-text">(555) 555-5555</p>',
-       '<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>', 
-      '<p class="modal-text">Birthday: 10/21/2015</p>', $modalBtnContainer);
-        
-      
-      
-      //Finally, I will append $modalBoxContainer inside body tag
-       $('body').append($modalBoxContainer);
-//==========================================================================================================================================
-  
-       
-    /*appends an "active" class to .modal(pop-up-window) and .modal-container(overlay) when .card is clicked
-       
-       I used a code snippet from https://www.pair.com/support/kb/how-to-use-jquery-to-generate-modal-pop-up-when-clicked/
-       */
-       $('.card').on("click", function() {
-           $(".modal, .modal-container").addClass("active");
-           console.log('the modal should pop up after clicking the div card')
-       });
-
-       /*This removes the "active" class to .modal(pop-up-window)  and .modal-container 
-       when clicking on: the "X" button, the opened modal or clicking outside the modal,
-       so the user has 3 ways to close a modal, this improves UX
-       */
-       $('#modal-close-btn, .modal, .modal-container').on("click", function()  {
-       $(".modal, .modal-container").removeClass("active");
-         console.log('you clicked on the x button');
-    }); 
-//==========================================================================================================================================
       
 /* 
   With information provided from The Random User Generator API https://randomuser.me/, 
@@ -113,72 +59,88 @@ Image
 First and Last Name
 Email
 City or location
+
+I received some help on this section from https://stackoverflow.com/a/53909635/10043628 Mohammad Raheem
   */
 
-  //this works, but all of gallery cards have the same pictures
+  
  $.ajax({
     url: 'https://randomuser.me/api/?nat=us&results=12&',
     dataType: 'json',
     success: function(data) {
       console.log(data); //this should log the data for 12 employees in JSON format
 
+      
+
       var employeeInfo = data.results //creating a reference to data.results
+      var _cardTemplate = ''; //make variable reference for gallery
+      var  modalBoxContainer = ''; //make variable for modal containers
+      $.each(employeeInfo, function (index, employee) {
+          //create variable references for Name, email, city,state, etc
+          var name = employee.name.first + " " + employee.name.last;
+          var email = employee.email;
+          var picture = employee.picture.large;
+          var location = employee.location.city;
+          var number = employee.phone;
+          var fullStreet = employee.location.street + " " + location + " " + employee.location.postcode;
+          var birthday = employee.dob.date;
 
-      $.each(employeeInfo, function(index, employee) {
-        //create variable references for Name, email, city,state, etc
-        var name = employee.name.first + " " + employee.name.last;
-        var email = employee.email;
-        var picture = employee.picture.large;
-        var location = employee.location.city;
-        var number = employee.phone;
-        var fullStreet = employee.location.street + " " + location + " " + employee.location.postcode;
-        var birthday =  employee.dob.date;
+          //CREATE GALLERY CARDS AND SHOW CONTENT FOR SMALL GALLERY CARDS
 
-        //SHOW CONTENT FOR SMALL GALLERY CARDS
+
+          _cardTemplate += '<div class="card">';
+          _cardTemplate += '<div class="card-img-container">';
+          _cardTemplate += '<img class="card-img" src= "' + picture + '" alt="profile picture"></div>';
+          _cardTemplate += '<div class="card-info-container"><h3 id="name" class="card-name cap">' + name + '</h3>';
+          _cardTemplate += '<p class="card-text">' + email + '</p><p class="card-text cap">' + location + '</p>';
+          _cardTemplate += '</div></div>';
+
+         
+         
+        //CREATE MODAL CARDS AND SHOW CONTENT FOR THEM
         
-        //attach images to employee gallery 
-        $('.card-img').attr('src', picture);
+        modalBoxContainer += '<div class="modal-container">';
+        modalBoxContainer += '<div class="modal">';
+        modalBoxContainer += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
+        modalBoxContainer += '<div class="modal-info-container"><img class="modal-img" src= "' + picture + '" alt="profile picture"><h3 id="name" class="modal-name cap">' + name + '</h3><p class="modal-text">' + email + '</p><p class="modal-text cap">' + location + '</p>';
+        modalBoxContainer += '<hr>';
+        modalBoxContainer += '<p class="modal-text">' + number + '</p><p class="modal-text">' + fullStreet + '</p><p class="modal-text">' + birthday + '</p></div>';
+        modalBoxContainer += '<div2>';
+         
 
-        //Get to display the name, I used a code snippet from https://stackoverflow.com/a/11468183/10043628 by user jagm
-        $('.card-info-container > :first-child').text(name);
-
-        //Get to display email
-        $('.card-text').text(email);
-
-        //Get to display city and state 
-        $('.card-info-container > :nth-child(3)').text(location);
-
-        
-        
-        //SHOW CONTENT FOR MODAL BOXES
-
-        //display name
-        $('.modal > :nth-child(2)').text(name);
-
-        //attach images to employee modals
-        $('.modal-img').attr('src', picture);
-
-        //Display email
-       $('.modal > :nth-child(3)').text(email);
-
-       //Display city
-        $('.modal > :nth-child(4)').text(location);
-
-        //Display number
-        $('.modal > :nth-child(6)').text(number);
-
-        //Display address
-        $('.modal > :nth-child(7)').text(fullStreet);
-
-        //Display Birthday
-        $('.modal > :nth-child(8)').text(birthday);
-
-        
-        
-      });
+        /*appends an "active" class to .modal(pop-up-window) and .modal-container(overlay) when .card is clicked
        
+       I used a code snippet from https://www.pair.com/support/kb/how-to-use-jquery-to-generate-modal-pop-up-when-clicked/
+       */
+       $('.card').on("click", function() {
+        $(".modal, .modal-container").addClass("active");
+        console.log('the modal should pop up after clicking the div card')
+    });
 
-} //closes success function
-    
-  }); //closes ajax request
+    /*This removes the "active" class to .modal(pop-up-window)  and .modal-container 
+    when clicking on: the "X" button, the opened modal or clicking outside the modal,
+    so the user has 3 ways to close a modal, this improves UX
+    */
+    $('#modal-close-btn, .modal, .modal-container').on("click", function()  {
+    $(".modal, .modal-container").removeClass("active");
+      console.log('you clicked on the x button');
+ }); 
+         
+          
+        
+        
 
+
+
+      });
+
+      $('#gallery').append(_cardTemplate); //Append Finally all cards with employee details
+         //Finally, I will append modalBoxContainer inside body tag
+        $('body').append(modalBoxContainer);
+  }
+
+})
+
+
+
+      
